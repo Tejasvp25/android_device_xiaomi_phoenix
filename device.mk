89137c57801ab05f44298647720fd9cb9cd9fb2f
lucky <ah440099@gmail.com>
2022-01-07 14:43:47 +0000

    phoenix: Build sensorservice from source

--- device.mk
+++ device.mk
@@ -558,7 +558,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0
 
 # Shims
 PRODUCT_PACKAGES += \
