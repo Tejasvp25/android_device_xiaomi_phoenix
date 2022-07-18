a196b4bac6b8b2e9c2be7c66eb9e949b800402ef
tejasvp25 <tejasvp25@gmail.com>
2022-07-18 13:59:20 +0530

    permissions: Add missing permissions
    * Fixes GPS issue
    
    Signed-off-by: tejasvp25 <tejasvp25@gmail.com>

--- device.mk
+++ device.mk
@@ -49,8 +49,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
    $(LOCAL_PATH)/configs/google-hiddenapi-package-whitelist-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/google-hiddenapi-package-whitelist-carriersettings.xml \
-   $(LOCAL_PATH)/permissions/privapp-permissions-google-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-carriersettings.xml
-
+   $(LOCAL_PATH)/permissions/privapp-permissions-google-carriersettings.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-carriersettings.xml \
+   $(LOCAL_PATH)/permissions/privapp-permissions-package-gms-extra.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-package-gms-extra.xml 
 # ATRACE_HAL
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service \
