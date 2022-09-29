e97c858e975a8220f55ed22c725dc7ba35254706
tejasvp25 <tejasvp25@gmail.com>
2022-09-29 18:06:00 +0530

    temp

--- device.mk
+++ device.mk
@@ -302,7 +302,9 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxEvrcEnc \
     libOmxG711Enc \
-    libOmxQcelp13Enc
+    libOmxQcelp13Enc \
+    libavservices_minijail.vendor \
+    android.hardware.media.c2@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
@@ -318,6 +320,55 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_omx.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vndk/libstagefright_omx.so \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_omx_utils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vndk/libstagefright_omx_utils.so
 
+
+# Enable Codec 2.0
+PRODUCT_PACKAGES += \
+    libqcodec2_base \
+    libqcodec2_utils \
+    libqcodec2_platform \
+    libqcodec2_core \
+    libqcodec2_basecodec \
+    libqcodec2_v4l2codec \
+    vendor.qti.media.c2@1.0-service \
+    codec2.vendor.ext-arm64.policy \
+    codec2.vendor.base-arm64.policy
+
+# Media codecs C2
+PRODUCT_PACKAGES += \
+    codec2play \
+    libcodec2_soft_aacdec \
+    libcodec2_soft_aacenc \
+    libcodec2_soft_amrnbdec \
+    libcodec2_soft_amrwbdec \
+    libcodec2_soft_amrnbenc \
+    libcodec2_soft_amrwbenc \
+    libcodec2_soft_av1dec_aom \
+    libcodec2_soft_avcdec \
+    libcodec2_soft_avcenc \
+    libcodec2_soft_common \
+    libcodec2_soft_flacdec \
+    libcodec2_soft_flacenc \
+    libcodec2_soft_g711alawdec \
+    libcodec2_soft_g711mlawdec \
+    libcodec2_soft_av1dec_gav1 \
+    libcodec2_soft_gsmdec \
+    libcodec2_soft_hevcdec \
+    libcodec2_soft_hevcenc \
+    libcodec2_soft_mp3dec \
+    libcodec2_soft_mpeg2dec \
+    libcodec2_soft_mpeg4dec \
+    libcodec2_soft_h263dec \
+    libcodec2_soft_mpeg4enc \
+    libcodec2_soft_h263enc \
+    libcodec2_soft_opusdec \
+    libcodec2_soft_opusenc \
+    libcodec2_soft_rawdec \
+    libcodec2_soft_vorbisdec \
+    libcodec2_soft_vp9dec \
+    libcodec2_soft_vp8dec \
+    libcodec2_soft_vp9enc \
+    libcodec2_soft_vp8enc \
+    libcodec2_soft_xaacdec
 
 # Media
 PRODUCT_PACKAGES += \
