839d2566db3537b8b30828dd0b6443661e395394
tejasvp25 <tejasvp25@gmail.com>
2023-04-28 21:45:05 +0530

    Revert "phoenix: Switch audio HAL to V7"
    This reverts commit 07edb622e3300b0219977d69b3dce353c0fdf99e.

--- device.mk
+++ device.mk
@@ -69,8 +69,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
     audio.usb.default \
